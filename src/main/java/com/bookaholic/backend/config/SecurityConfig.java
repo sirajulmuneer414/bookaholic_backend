50bee9a88955b959602e41bd5af980e866b9875d
Sirajul Muneer <muneersirajul414@gmail.com>
2026-02-05 20:25:26 +0530

    added url to secConfig

--- src/main/java/com/bookaholic/backend/config/SecurityConfig.java
+++ src/main/java/com/bookaholic/backend/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://bookaholic-frontend.vercel.app","http://localhost:5173","http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
