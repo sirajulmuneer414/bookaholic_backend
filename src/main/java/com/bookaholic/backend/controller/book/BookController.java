b6b13a550f1124f291fe74d829e3c70922390e9d
Sirajul Muneer <muneersirajul414@gmail.com>
2026-02-06 10:27:38 +0530

    small change in book controller

--- src/main/java/com/bookaholic/backend/controller/book/BookController.java
+++ src/main/java/com/bookaholic/backend/controller/book/BookController.java
@@ -13,6 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+// Book Controller 
+
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
